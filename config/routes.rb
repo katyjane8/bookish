c8612f544a6c56f73b094d485315f68d1b84447b
katyjane8 <katy.jane8@gmail.com>
2018-04-03 09:46:47 -0600

    user dashboard route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: redirect('/')
+  get 'logout', to: 'sessions#destroy'
+
+  get 'dashboard', to: 'dashboard#show'
 end
