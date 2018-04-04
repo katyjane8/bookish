c52551379f69d32b2ef460664897065cd935a3b4
katyjane8 <katy.jane8@gmail.com>
2018-04-04 11:56:35 -0600

    dashboard controller now has the Faraday call instead of the view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   def index
+    response = Faraday.get(URI.parse("https://www.goodreads.com/review/list/2518130.xml?key=#{ENV["GOODREADS_CLIENT_ID"]}&v=2")).body.force_encoding 'UTF-8'
+    raw_data = Hash.from_xml(response)
+    @book_info = raw_data["GoodreadsResponse"]["reviews"]["review"].map do |result|
+      Goodreads.new(result["book"])
+    end
   end
 end
