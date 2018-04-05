d2bc56a98d419764765d9bb041bdac69dbdf7fe3
katyjane8 <katy.jane8@gmail.com>
2018-04-05 11:25:31 -0600

    attempting omniauth goodreads gem with strategy

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get 'auth/:provider/callback', to: 'sessions#create'
-  get 'auth/failure', to: redirect('/')
-  get 'logout', to: 'sessions#destroy'
+  get '/auth/goodreads', to: 'sessions#new'
+  get '/auth/goodreads/callback', to: 'sessions#create'
+  get '/auth/failure', to: redirect('/')
+  get '/logout', to: 'sessions#destroy'
 
-  get 'dashboard', to: 'dashboard#index'
+  get '/dashboard', to: 'dashboard#index'
 end
