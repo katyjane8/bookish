10ed68ae6133efdcab69e407acc3af6a93565ec3
katyjane8 <katy.jane8@gmail.com>
2018-04-05 10:22:41 -0600

    session new with goodreads token

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
     response = Faraday.get(URI.parse("https://www.goodreads.com/review/list/2518130.xml?key=#{ENV["GOODREADS_CLIENT_ID"]}&v=2")).body.force_encoding 'UTF-8'
     raw_data = Hash.from_xml(response)
     @book_info = raw_data["GoodreadsResponse"]["reviews"]["review"].map do |result|
-      Goodreads.new(result["book"])
+      BookQueue.new(result["book"])
     end
   end
 end
