d52a01fd1a3f855c8d54744b2e65757030ae2ee8
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-06 17:21:55 -0600

    working with dashboard view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    # client_id     = ENV["GOODREADS_CLIENT_ID"]
-    # client_secret = ENV["GOODREADS_SECRET"]
-    # token          = params[:oauth_token]
-    # goodreads_client = User.new(token)
-    # goodreads_client.save!
-    
+    user = User.update_or_create(params["oauth_token"])
+      session[:id] = user.id
+
     redirect_to dashboard_path
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,7 @@
 class User < ApplicationRecord
-  def initialize(token)
-    @token = token
+  def self.update_or_create(params)
+    user = User.new || User.find_by(id: params[:id])
+    user.save!
+    user
   end
-  # def self.update_or_create(auth)
-  #   user = User.find_by(id: auth[:id]) || User.new
-  # #   user.attributes = {
-  # #   provider: auth[:provider],
-  # #   uid: auth[:uid],
-  # #   email: auth[:info][:email],
-  # #   first_name: auth[:info][:first_name],
-  # #   last_name: auth[:info][:last_name],
-  # #   token: auth[:credentials][:token],
-  # #   refresh_token: auth[:credentials][:refresh_token],
-  # #   oauth_expires_at: auth[:credentials][:expires_at]
-  # # }
-  #   user.save!
-  #   user
-  # end
 end
