0a367ac522848e76c1bfeb7a87963b3cf0586810
katyjane8 <katy.jane8@gmail.com>
2018-04-05 18:26:54 -0600

    Goodreads Oauth WIP

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    client_id     = ENV["GOODREADS_CLIENT_ID"]
-    client_secret = ENV["GOODREADS_SECRET"]
-    token          = params[:oauth_token]
-    goodreads_client = User.new(token)
-    goodreads_client.save!
+    # client_id     = ENV["GOODREADS_CLIENT_ID"]
+    # client_secret = ENV["GOODREADS_SECRET"]
+    # token          = params[:oauth_token]
+    # goodreads_client = User.new(token)
+    # goodreads_client.save!
     
     redirect_to dashboard_path
   end
