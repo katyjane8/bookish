89835cf0b1842e4cd5447ff60d47907f9abb5c3f
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-06 11:52:19 -0600

    a little js to flip book covers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,15 @@
 //
 //= require rails-ujs
 //= require_tree .
+//= require jquery
+//= require jquery_ujs
+$(document).ready(function(){
+  // set up hover panels
+  // although this can be done without JavaScript, we've attached these events
+  // because it causes the hover to be triggered when the element is tapped on a touch device
+  $('.hover').hover(function(){
+    $(this).addClass('flip');
+  },function(){
+    $(this).removeClass('flip');
+  });
+});
