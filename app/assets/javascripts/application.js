a876172ac92bb4837ab8245b812d09225e9250ac
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-09 10:04:22 -0600

    clobber assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require_tree .
 //= require jquery
 //= require jquery_ujs
+//= require_tree .
 $(document).ready(function(){
   // set up hover panels
   // although this can be done without JavaScript, we've attached these events
