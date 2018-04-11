d70d3506eef65c5d6ee6d0f9cd7470f800c75e0d
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-10 19:10:25 -0600

    tried some conditionals for current_user to make refresh work

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class DashboardController < ApplicationController
   def index
-    @book_info = token_generator
+    if current_user
+      @book_info = token_generator
+    end
 
     response = Faraday.get("http://api.nytimes.com/svc/books/v2/lists.json?api-key=#{ENV["NYT_KEY"]}&list=mass-market-paperback")
     raw_data = JSON.parse(response.body, symbolize_headers: true)
