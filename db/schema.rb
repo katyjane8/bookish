593cf713c64ba1691bdfec85ca645c9cb167f1f4
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-10 19:11:53 -0600

    new schema to try and save books to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180402215832) do
+ActiveRecord::Schema.define(version: 20180410220507) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "hstore"
+
+  create_table "books", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "isbn"
+    t.string "title"
+    t.string "image_url"
+    t.string "small_image_url"
+    t.string "link"
+    t.string "num_pages"
+    t.string "format"
+    t.string "publisher"
+    t.string "publication_year"
+    t.string "average_rating"
+    t.string "ratings_count"
+    t.hstore "description"
+    t.hstore "authors"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "oauth_token"
@@ -21,4 +42,5 @@ ActiveRecord::Schema.define(version: 20180402215832) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "users"
 end
