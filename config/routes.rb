0715736162ccb135a0e5bc0d4e0f243aa795d9d7
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-12 21:21:36 -0600

    route for nyt bestsellers

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/auth/goodreads', to: 'sessions#new'
   get '/auth/goodreads/callback', to: 'sessions#create'
   get '/auth/failure', to: redirect('/')
+  get '/bestsellers', to: 'bestsellers#index'
   get '/logout', to: 'sessions#destroy'
 
   get '/dashboard', to: 'dashboard#index'
