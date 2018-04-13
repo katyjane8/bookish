ed51941147f15eb571d978bcc1a668ba1ab419cd
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-12 21:20:49 -0600

    bestsellers has own view now - need to pull in another call for hardcover fiction

---
+++ app/controllers/bestsellers_controller.rb
@@ -0,0 +1,9 @@
+class BestsellersController < ApplicationController
+  def index
+    response = Faraday.get("http://api.nytimes.com/svc/books/v2/lists.json?api-key=#{ENV["NYT_KEY"]}&list=mass-market-paperback")
+    raw_data = JSON.parse(response.body, symbolize_headers: true)
+    @nyt_reviews = raw_data["results"].map do |result|
+      Reviews.new(result)
+    end
+  end
+end
