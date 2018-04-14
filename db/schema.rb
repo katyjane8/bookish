17af25a35b74c397f621a4df9844aa24f570ddf1
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-14 14:26:43 -0600

    add links, name and uid to user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410220507) do
+ActiveRecord::Schema.define(version: 20180414162542) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,10 +36,25 @@ ActiveRecord::Schema.define(version: 20180410220507) do
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
+  create_table "bookstores", force: :cascade do |t|
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.float "latitude"
+    t.float "longitude"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "oauth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "name"
+    t.string "link"
   end
 
   add_foreign_key "books", "users"
