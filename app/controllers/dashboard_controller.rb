12b063d3ea03ec37a38a1caa672275a5eb999f8e
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-14 14:17:02 -0600

    refactored a service in here

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,12 @@
 class DashboardController < ApplicationController
   def index
-    if current_user
-      @book_info = token_generator
+    if current_user.uid != nil
+      @book_queue_service = BookQueueService.new(current_user)
+      @book_info = @book_queue_service.get_book_queue
+    else
+      get_user_info
+      @book_queue_service = BookQueueService.new(current_user)
+      @book_info = @book_queue_service.get_book_queue
     end
   end
 end
