51c846ffb73afd1906376087ae8cbb02f698c019
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-17 09:39:57 -0600

    bestseller cleanup with service

--- app/controllers/bestsellers_controller.rb
+++ app/controllers/bestsellers_controller.rb
@@ -1,15 +1,8 @@
 class BestsellersController < ApplicationController
   def index
-    response = Faraday.get("http://api.nytimes.com/svc/books/v2/lists.json?api-key=#{ENV["NYT_KEY"]}&list=mass-market-paperback")
-    raw_data = JSON.parse(response.body, symbolize_headers: true)
-    @nyt_reviews = raw_data["results"].map do |result|
-      Reviews.new(result)
-    end
-
-    hardcover = Faraday.get("http://api.nytimes.com/svc/books/v2/lists.json?api-key=#{ENV["NYT_KEY"]}&list=hardcover-fiction")
-    raw_data = JSON.parse(hardcover.body, symbolize_headers: true)
-    @nyt_hc_reviews = raw_data["results"].map do |result|
-      Reviews.new(result)
-    end
+    list = BestsellerService.new
+    @paperbacks = list.paperback
+    @hardcovers = list.hardcover
   end
+
 end
