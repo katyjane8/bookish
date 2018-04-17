12058f7c29e0cfb539a86459e2e4039d6ca0294a
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-17 09:41:01 -0600

    bestseller service

---
+++ app/services/bestseller_service.rb
@@ -0,0 +1,35 @@
+class BestsellerService
+  def initialize
+  end
+
+  def conn
+    Faraday.new("http://api.nytimes.com") do |f|
+      f.request  :url_encoded
+      f.headers["api-key"] = ENV["NYT_KEY"]
+    end
+  end
+
+  def paperback
+    response = Faraday.get("http://api.nytimes.com/svc/books/v2/lists.json?api-key=#{ENV["NYT_KEY"]}&list=mass-market-paperback")
+    # response = conn.get("svc/books/v2/lists.json?&list=mass-market-paperback")
+    raw_data = JSON.parse(response.body, symbolize_headers: true)
+    raw_data["results"].map do |result|
+      Reviews.new(result)
+    end
+  end
+
+  def hardcover
+    response = Faraday.get("http://api.nytimes.com/svc/books/v2/lists.json?api-key=#{ENV["NYT_KEY"]}&list=hardcover-fiction")
+    # conn.get('hardcover-fiction')
+    raw_data = JSON.parse(response.body, symbolize_headers: true)
+    raw_data["results"].map do |result|
+      Reviews.new(result)
+    end
+  end
+
+    private
+
+    def parse_json(response)
+      JSON.parse(response.body, symbolize_headers: true)
+    end
+end
