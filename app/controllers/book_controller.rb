2c57e56fb205be6b4176497bf673bf297161043c
Kathrine Welyczko <kathrine@KatBook-Pro.local>
2018-04-18 13:39:10 -0600

    controller for book_show pages

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -1,4 +1,15 @@
 class BookController < ApplicationController
-  def show
+  before_action :set_book
+
+  def index
+    response = Faraday.get("https://www.goodreads.com/book/title.xml?key=#{ENV["GOODREADS_CLIENT_ID"]}&title=#{@book.last.title}").body.force_encoding 'UTF-8'
+    raw_data = Hash.from_xml(response)
+    @book_show = BookInfo.new(raw_data["GoodreadsResponse"]["book"])
   end
+
+    private
+
+    def set_book
+      @book = Book.all
+    end
 end
